9d10fced7902316582f4d32c0e5616d2e0e80e74
Abhishek Oza <abhishek@itoneclick.in>
2023-03-16 11:41:18 +0530

    fit : dao-DataAccessObject.(16th march 23, 11:41)

--- app/src/main/java/com/example/cleanarchitecturedemo/data/MovieDao.kt
+++ app/src/main/java/com/example/cleanarchitecturedemo/data/MovieDao.kt
@@ -1,4 +1,19 @@
 package com.example.cleanarchitecturedemo.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.cleanarchitecturedemo.data.model.movie.Movie
+
+@Dao
 interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMoviesToDB(movies: List<Movie>)
+
+    @Query(value = "DELETE FROM movie_db")
+    suspend fun deleteMovies()
+
+    @Query("SELECT * FROM movie_db")
+    suspend fun getMoviesFromDB(movies: List<Movie>)
 }
